c3d854c03c78bb798ffe7f1efa0d45fe94bbd5f0
Mikecito080 <134610551+Mikecito080@users.noreply.github.com>
2025-04-22 04:13:20 +0000

    8

--- js/filtro.js
+++ js/filtro.js
@@ -6,7 +6,7 @@ export function loadUsuario() {
     <p><strong>API utilizada:</strong> JokeAPI (<a href="https://jokeapi.dev/" target="_blank">jokeapi.dev</a>)</p>
     <p><strong>GitHub:</strong> <a href="https://github.com/Mikecito080" target="_blank">@Mikecito080</a></p>
     <p><strong>Versión:</strong> V.1.0.0</p>
-    <img src="https://i.imgur.com/ZPOeY3F.png" alt="Humor" style="max-width: 300px; margin-top: 10px;">
+    <img src="img/stand.jpg" alt="Humor" style="max-width: 300px; margin-top: 10px;">
     <p><strong>Descripción:</strong> Esta app permite visualizar chistes de todo tipo, categorizarlos, guardarlos como favoritos, y explorar contenido aleatorio de la JokeAPI.</p>
   `;
   
